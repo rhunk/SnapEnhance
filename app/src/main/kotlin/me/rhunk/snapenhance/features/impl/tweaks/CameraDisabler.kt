b5950d77d5a3e748780c6d7172bffabdb81bfb29
rhunk <101876869+rhunk@users.noreply.github.com>
2023-06-09 03:11:38 +0200

    feat: camera disabler

--- app/src/main/kotlin/me/rhunk/snapenhance/features/impl/tweaks/CameraDisabler.kt
+++ app/src/main/kotlin/me/rhunk/snapenhance/features/impl/tweaks/CameraDisabler.kt
@@ -1,41 +1,18 @@
 package me.rhunk.snapenhance.features.impl.tweaks
 
-import android.Manifest
-import android.app.AppOpsManager
 import android.app.admin.DevicePolicyManager
-import android.content.pm.PackageManager
-import android.widget.Toast
 import me.rhunk.snapenhance.config.ConfigProperty
 import me.rhunk.snapenhance.features.Feature
 import me.rhunk.snapenhance.features.FeatureLoadParams
 import me.rhunk.snapenhance.hook.HookStage
 import me.rhunk.snapenhance.hook.Hooker
 
-class CameraDisabler : Feature("Camera Disabler", loadParams = FeatureLoadParams.ACTIVITY_CREATE_ASYNC){
-    override fun asyncOnActivityCreate() {
-        //TODO: Implement a hook that hooks the camera permission check instead of this
-        val policyManager = DevicePolicyManager::class.java
+class CameraDisabler : Feature("Camera Disabler", loadParams = FeatureLoadParams.ACTIVITY_CREATE_SYNC) {
+    override fun onActivityCreate() {
         if (!context.config.bool(ConfigProperty.CAMERA_DISABLE)) return
-        Hooker.hook(policyManager, "getCameraDisabled", HookStage.BEFORE) { param ->
-            param.setResult(true)
+        //TODO: (?) Implement a hook that hooks the camera permission check instead of this
+        Hooker.hook(DevicePolicyManager::class.java, "getCameraDisabled", HookStage.BEFORE) {
+            it.setResult(true)
         }
-        
-        /*
-        val packageManager = PackageManager::class.java
-        Hooker.hook(packageManager, "checkSelfPermission", HookStage.BEFORE) { param ->
-            val perm = param.args()[0] as String
-            Toast.makeText(context.mainActivity!!, perm, Toast.LENGTH_LONG).show() //DEBUG
-            if(perm != Manifest.permission.CAMERA) return@hook
-            param.setResult(PackageManager.PERMISSION_GRANTED)
-        }
-        
-        val appOpsManager = AppOpsManager::class.java
-        Hooker.hook(appOpsManager, "checkOp", HookStage.BEFORE) { param ->
-            if(param.args()[0] != AppOpsManager.OPSTR_CAMERA) return@hook
-            param.setResult(AppOpsManager.MODE_ALLOWED)
-        }
-        */
-        
     }
-    
 }
