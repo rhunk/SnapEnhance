0e940dfa3fbcea20928b9a46bd23866897638b0d
rhunk <101876869+rhunk@users.noreply.github.com>
2023-06-09 02:21:50 +0200

    refact: config enum keys

--- app/src/main/kotlin/me/rhunk/snapenhance/features/impl/ConfigEnumKeys.kt
+++ app/src/main/kotlin/me/rhunk/snapenhance/features/impl/ConfigEnumKeys.kt
@@ -6,31 +6,48 @@ import me.rhunk.snapenhance.features.Feature
 import me.rhunk.snapenhance.features.FeatureLoadParams
 import me.rhunk.snapenhance.hook.HookStage
 import me.rhunk.snapenhance.hook.hook
+import me.rhunk.snapenhance.util.getObjectField
 import me.rhunk.snapenhance.util.setObjectField
 import java.lang.reflect.Field
 import java.lang.reflect.Modifier
+import java.lang.reflect.Type
 
 class ConfigEnumKeys : Feature("Config enum keys", loadParams = FeatureLoadParams.ACTIVITY_CREATE_SYNC) {
-    private fun hookAllEnums(enumClass: Class<*>, callback: (String, (Any) -> Unit) -> Unit) {
-        //Enum(String, int, ?)
-        //or Enum(?)
-        val enumDataClass = enumClass.constructors[0].parameterTypes.first { clazz: Class<*> -> clazz != String::class.java && !clazz.isPrimitive }
 
-        //get the field which contains the enum data class
-        val enumDataField = enumClass.declaredFields.first { field: Field -> field.type == enumDataClass }
+    data class HookEnumContext(
+        val key: String,
+        val type: Type?,
+        val value: Any?,
+        val set: (Any) -> Unit
+    )
 
-        //get the field value of the enum data class (the first field of the class with the desc Object)
-        val objectDataField = enumDataField.type.fields.first { field: Field ->
-            field.type == Any::class.java && Modifier.isPublic(
-                field.modifiers
-            ) && Modifier.isFinal(field.modifiers)
-        }
+    companion object {
+        fun hookAllEnums(enumClass: Class<*>, callback: HookEnumContext.() -> Unit) {
+            //Enum(String, int, ?)
+            //or Enum(?)
+            val enumDataClass = enumClass.constructors[0].parameterTypes.first { clazz: Class<*> -> clazz != String::class.java && !clazz.isPrimitive }
 
-        enumClass.enumConstants.forEach { enum ->
-            enumDataField.get(enum)?.let { enumData ->
-                val key = enum.toString()
-                callback(key) { newValue ->
-                    enumData.setObjectField(objectDataField.name, newValue)
+            //get the field which contains the enum data class
+            val enumDataField = enumClass.declaredFields.first { field: Field -> field.type == enumDataClass }
+
+            val typeField = enumDataClass.declaredFields.first { field: Field -> field.type == Type::class.java }
+
+            //get the field value of the enum data class (the first field of the class with the desc Object)
+            val objectDataField = enumDataField.type.fields.first { field: Field ->
+                field.type == Any::class.java && Modifier.isPublic(
+                    field.modifiers
+                ) && Modifier.isFinal(field.modifiers)
+            }
+
+            enumClass.enumConstants.forEach { enum ->
+                enumDataField.get(enum)?.let { enumData ->
+                    val key = enum.toString()
+                    val type = typeField.get(enumData) as Type?
+                    val value = enumData.getObjectField(objectDataField.name)
+                    val set = { newValue: Any ->
+                        enumData.setObjectField(objectDataField.name, newValue)
+                    }
+                    callback(HookEnumContext(key, type, value, set))
                 }
             }
         }
@@ -39,25 +56,25 @@ class ConfigEnumKeys : Feature("Config enum keys", loadParams = FeatureLoadParam
     @SuppressLint("PrivateApi")
     override fun onActivityCreate() {
         if (context.config.bool(ConfigProperty.NEW_MAP_UI)) {
-            hookAllEnums(context.mappings.getMappedClass("enums", "PLUS")) { key, set ->
+            hookAllEnums(context.mappings.getMappedClass("enums", "PLUS")) {
                 if (key == "REDUCE_MY_PROFILE_UI_COMPLEXITY") set(true)
             }
         }
 
-        hookAllEnums(context.mappings.getMappedClass("enums", "ARROYO")) { key, set ->
+        hookAllEnums(context.mappings.getMappedClass("enums", "ARROYO")) {
             if (key == "ENABLE_LONG_SNAP_SENDING") {
                 if (context.config.bool(ConfigProperty.DISABLE_SNAP_SPLITTING)) set(true)
             }
         }
 
         if (context.config.bool(ConfigProperty.STREAK_EXPIRATION_INFO)) {
-            hookAllEnums(context.mappings.getMappedClass("enums", "FRIENDS_FEED")) { key, set ->
+            hookAllEnums(context.mappings.getMappedClass("enums", "FRIENDS_FEED")) {
                 if (key == "STREAK_EXPIRATION_INFO") set(true)
             }
         }
 
         if (context.config.bool(ConfigProperty.BLOCK_ADS)) {
-            hookAllEnums(context.mappings.getMappedClass("enums", "SNAPADS")) { key, set ->
+            hookAllEnums(context.mappings.getMappedClass("enums", "SNAPADS")) {
                 if (key == "BYPASS_AD_FEATURE_GATE") {
                     set(true)
                 }
