9a62713fa11afa6c49f8148bf14c1f8da8ca6c4d
rhunk <101876869+rhunk@users.noreply.github.com>
2023-06-28 14:28:10 +0200

    fix(context): storage permission for legacy devices

--- app/src/main/kotlin/me/rhunk/snapenhance/SharedContext.kt
+++ app/src/main/kotlin/me/rhunk/snapenhance/SharedContext.kt
@@ -7,7 +7,6 @@ import android.content.Intent
 import android.os.Build
 import android.os.Environment
 import android.provider.Settings
-import androidx.annotation.RequiresApi
 import me.rhunk.snapenhance.bridge.TranslationWrapper
 import me.rhunk.snapenhance.download.DownloadTaskManager
 import kotlin.system.exitProcess
@@ -19,16 +18,22 @@ object SharedContext {
     lateinit var downloadTaskManager: DownloadTaskManager
     lateinit var translation: TranslationWrapper
 
-    @RequiresApi(Build.VERSION_CODES.R)
     private fun askForStoragePermission(context: Context) {
-        val intent = Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION)
-        intent.addCategory("android.intent.category.DEFAULT")
-        intent.data = android.net.Uri.parse("package:${context.packageName}")
-        if (context !is Activity) {
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            val intent = Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION)
+            intent.addCategory("android.intent.category.DEFAULT")
+            intent.data = android.net.Uri.parse("package:${context.packageName}")
+            if (context !is Activity) {
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            }
+            context.startActivity(intent)
+            exitProcess(0)
         }
-        context.startActivity(intent)
-        exitProcess(0)
+        if (context !is Activity) {
+            Logger.log("Storage permission not granted, exiting")
+            exitProcess(0)
+        }
+        context.requestPermissions(arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE, android.Manifest.permission.READ_EXTERNAL_STORAGE), 0)
     }
 
     fun ensureInitialized(context: Context) {
@@ -44,22 +49,27 @@ object SharedContext {
         }
 
         //ask for storage permission
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            if (Environment.isExternalStorageManager()) return
-            if (context !is Activity) {
-                askForStoragePermission(context)
-                return
-            }
-            AlertDialog.Builder(context)
-                .setTitle("Storage permission")
-                .setMessage("App needs storage permission to download files and save them to your device. Please allow it in the next screen.")
-                .setPositiveButton("Grant") { _, _ ->
-                    askForStoragePermission(context)
-                }
-                .setNegativeButton("Cancel") { _, _ ->
-                    exitProcess(0)
-                }
-                .show()
+        val hasStoragePermission = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            Environment.isExternalStorageManager()
+        } else {
+            context.checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE) == android.content.pm.PackageManager.PERMISSION_GRANTED
         }
+
+        if (hasStoragePermission) return
+
+        if (context !is Activity) {
+            askForStoragePermission(context)
+            return
+        }
+        AlertDialog.Builder(context)
+            .setTitle("Storage permission")
+            .setMessage("App needs storage permission to download files and save them to your device. Please allow it in the next screen.")
+            .setPositiveButton("Grant") { _, _ ->
+                askForStoragePermission(context)
+            }
+            .setNegativeButton("Cancel") { _, _ ->
+                exitProcess(0)
+            }
+            .show()
     }
 }
