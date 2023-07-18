197e8e86270a2100d9cb9f59f641fe2f40ad5611
rhunk <101876869+rhunk@users.noreply.github.com>
2023-07-18 10:28:21 +0200

    fix: screenshot detection in snaps

--- app/src/main/kotlin/me/rhunk/snapenhance/features/impl/privacy/PreventMessageSending.kt
+++ app/src/main/kotlin/me/rhunk/snapenhance/features/impl/privacy/PreventMessageSending.kt
@@ -7,19 +7,29 @@ import me.rhunk.snapenhance.data.wrapper.impl.MessageContent
 import me.rhunk.snapenhance.features.Feature
 import me.rhunk.snapenhance.features.FeatureLoadParams
 import me.rhunk.snapenhance.hook.HookStage
-import me.rhunk.snapenhance.hook.Hooker
+import me.rhunk.snapenhance.hook.hook
 
 class PreventMessageSending : Feature("Prevent message sending", loadParams = FeatureLoadParams.ACTIVITY_CREATE_ASYNC) {
     override fun asyncOnActivityCreate() {
-        Hooker.hook(
-            context.classCache.conversationManager,
-            "sendMessageWithContent",
-            HookStage.BEFORE
-        ) { param ->
+        context.classCache.conversationManager.hook("updateMessage", HookStage.BEFORE) { param ->
+            val messageUpdate = param.arg<Any>(2).toString();
+            val options by lazy {
+                context.config.options(ConfigProperty.PREVENT_SENDING_MESSAGES)
+            }
+            if (messageUpdate == "SCREENSHOT" && options["chat_screenshot"] == true) {
+                param.setResult(null)
+            }
+
+            if (messageUpdate == "SCREEN_RECORD" && options["chat_screen_record"] == true) {
+                param.setResult(null)
+            }
+        }
+
+        context.classCache.conversationManager.hook("sendMessageWithContent", HookStage.BEFORE) { param ->
             val message = MessageContent(param.arg(1))
             val contentType = message.contentType
-            val options = context.config.options(ConfigProperty.PREVENT_SENDING_MESSAGES)
             val associatedType = NotificationType.fromContentType(contentType) ?: return@hook
+            val options = context.config.options(ConfigProperty.PREVENT_SENDING_MESSAGES)
 
             if (options[associatedType.key] == true) {
                 Logger.debug("Preventing message sending for $associatedType")
