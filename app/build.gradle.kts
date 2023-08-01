a3afdc5000be8911f5ec54ae8cea98f4b6122a2a
auth <64337177+authorisation@users.noreply.github.com>
2023-08-01 12:30:24 +0000

    bump version (v1.2.1)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,8 +6,8 @@ plugins {
     alias(libs.plugins.kotlinAndroid)
 }
 
-val appVersionName = "1.1.0"
-val appVersionCode = 7
+val appVersionName = "1.2.1"
+val appVersionCode = 9
 
 android {
     namespace = "me.rhunk.snapenhance"
