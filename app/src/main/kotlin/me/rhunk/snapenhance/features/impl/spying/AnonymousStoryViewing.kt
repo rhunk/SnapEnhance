9dc61b62158b84a50149632c7ea95a12d4b3be73
rhunk <101876869+rhunk@users.noreply.github.com>
2023-08-08 00:49:21 +0200

    fix: anonymous story viewing

--- app/src/main/kotlin/me/rhunk/snapenhance/features/impl/spying/AnonymousStoryViewing.kt
+++ app/src/main/kotlin/me/rhunk/snapenhance/features/impl/spying/AnonymousStoryViewing.kt
@@ -6,14 +6,15 @@ import me.rhunk.snapenhance.features.FeatureLoadParams
 import me.rhunk.snapenhance.hook.HookStage
 import me.rhunk.snapenhance.hook.Hooker
 import me.rhunk.snapenhance.util.getObjectField
+import me.rhunk.snapenhance.util.setObjectField
 
 class AnonymousStoryViewing : Feature("Anonymous Story Viewing", loadParams = FeatureLoadParams.ACTIVITY_CREATE_ASYNC) {
     override fun asyncOnActivityCreate() {
         Hooker.hook(context.classCache.networkApi,"submit", HookStage.BEFORE, { context.config.bool(ConfigProperty.ANONYMOUS_STORY_VIEW) }) {
             val httpRequest: Any = it.arg(0)
             val url = httpRequest.getObjectField("mUrl") as String
-            if (url.endsWith("readreceipt-indexer/batchuploadreadreceipts") || url.endsWith("v2/batch_cta")) {
-                it.setResult(null)
+            if (url.endsWith("readreceipt-indexer/batchuploadreadreceipts")) {
+                httpRequest.setObjectField("mUrl", "http://127.0.0.1")
             }
         }
     }
