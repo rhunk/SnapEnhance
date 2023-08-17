a8c4c1ce3eaf3b548a8bf88f567b66c5cb5efa45
rhunk <101876869+rhunk@users.noreply.github.com>
2023-08-17 17:44:20 +0200

    fix(mapper): story boost state

--- mapper/src/main/kotlin/me/rhunk/snapmapper/impl/StoryBoostStateMapper.kt
+++ mapper/src/main/kotlin/me/rhunk/snapmapper/impl/StoryBoostStateMapper.kt
@@ -3,8 +3,6 @@ package me.rhunk.snapmapper.impl
 import me.rhunk.snapmapper.AbstractClassMapper
 import me.rhunk.snapmapper.MapperContext
 import me.rhunk.snapmapper.ext.findConstString
-import me.rhunk.snapmapper.ext.getStaticConstructor
-import me.rhunk.snapmapper.ext.isEnum
 
 class StoryBoostStateMapper : AbstractClassMapper() {
     override fun run(context: MapperContext) {
@@ -13,9 +11,7 @@ class StoryBoostStateMapper : AbstractClassMapper() {
             if (firstConstructor.parameters.size != 3) continue
             if (firstConstructor.parameterTypes[1] != "J" || firstConstructor.parameterTypes[2] != "J") continue
 
-            val storyBoostEnumClass = context.getClass(firstConstructor.parameterTypes[0]) ?: continue
-            if (!storyBoostEnumClass.isEnum()) continue
-            if (storyBoostEnumClass.getStaticConstructor()?.implementation?.findConstString("NeedSubscriptionCannotSubscribe") != true) continue
+            if (clazz.methods.firstOrNull { it.name == "toString" }?.implementation?.findConstString("StoryBoostState", contains = true) != true) continue
 
             context.addMapping("StoryBoostStateClass", clazz.type.replace("L", "").replace(";", ""))
             return
