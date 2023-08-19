7854b167dde4a6ed0f56dfa6c8c4af642742e8e6
rhunk <101876869+rhunk@users.noreply.github.com>
2023-08-19 12:09:33 +0200

    Merge branch 'main' into refactor_2_0_0 - fix(media_downloader): download from topics - fix: anonymous story viewing

--- core/src/main/kotlin/me/rhunk/snapenhance/features/impl/spying/AnonymousStoryViewing.kt
+++ core/src/main/kotlin/me/rhunk/snapenhance/features/impl/spying/AnonymousStoryViewing.kt
@@ -5,6 +5,7 @@ import me.rhunk.snapenhance.features.FeatureLoadParams
 import me.rhunk.snapenhance.hook.HookStage
 import me.rhunk.snapenhance.hook.Hooker
 import me.rhunk.snapenhance.util.ktx.getObjectField
+import me.rhunk.snapenhance.util.ktx.setObjectField
 
 class AnonymousStoryViewing : Feature("Anonymous Story Viewing", loadParams = FeatureLoadParams.ACTIVITY_CREATE_ASYNC) {
     override fun asyncOnActivityCreate() {
@@ -12,8 +13,8 @@ class AnonymousStoryViewing : Feature("Anonymous Story Viewing", loadParams = Fe
         Hooker.hook(context.classCache.networkApi,"submit", HookStage.BEFORE, { anonymousStoryViewProperty }) {
             val httpRequest: Any = it.arg(0)
             val url = httpRequest.getObjectField("mUrl") as String
-            if (url.endsWith("readreceipt-indexer/batchuploadreadreceipts") || url.endsWith("v2/batch_cta")) {
-                it.setResult(null)
+            if (url.endsWith("readreceipt-indexer/batchuploadreadreceipts")) {
+                httpRequest.setObjectField("mUrl", "http://127.0.0.1")
             }
         }
     }
