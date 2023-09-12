6d730860b7afc4e4a9dc77fce8e550a9fedfec92
rhunk <101876869+rhunk@users.noreply.github.com>
2023-09-12 18:45:51 +0200

    fix(mapper): exclude CppProxy classes from callbacks

--- mapper/src/main/kotlin/me/rhunk/snapmapper/impl/CallbackMapper.kt
+++ mapper/src/main/kotlin/me/rhunk/snapmapper/impl/CallbackMapper.kt
@@ -12,12 +12,13 @@ class CallbackMapper : AbstractClassMapper() {
             if (clazz.superclass == null) return@filter false
 
             val superclassName = clazz.getSuperClassName()!!
-            if (!superclassName.endsWith("Callback") || superclassName.endsWith("\$Callback")) return@filter false
+            if ((!superclassName.endsWith("Callback") && !superclassName.endsWith("Delegate"))
+                || superclassName.endsWith("\$Callback")) return@filter false
+
+            if (clazz.getClassName().endsWith("\$CppProxy")) return@filter false
 
             val superClass = context.getClass(clazz.superclass) ?: return@filter false
-            if (superClass.isFinal()) return@filter false
-
-            superClass.virtualMethods.any { it.name == "onError" }
+            !superClass.isFinal()
         }.map {
              it.getSuperClassName()!!.substringAfterLast("/") to it.getClassName()
         }
