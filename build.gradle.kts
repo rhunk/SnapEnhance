e6f4a109dad8e7db0191f3faa99d686e56590477
rhunk <101876869+rhunk@users.noreply.github.com>
2023-10-07 11:40:30 +0200

    build: custom native name property

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,4 @@ plugins {
 rootProject.ext.set("appVersionName", "2.0.0")
 rootProject.ext.set("appVersionCode", 10)
 rootProject.ext.set("applicationId", "me.rhunk.snapenhance")
-rootProject.ext.set("nativeName", java.security.SecureRandom().nextLong(1000000000, 99999999999).toString(16))
-
-true // Needed to make the Suppress annotation work for the plugins block
+rootProject.ext.set("nativeName", properties["custom_native_name"] ?: java.security.SecureRandom().nextLong(1000000000, 99999999999).toString(16))
