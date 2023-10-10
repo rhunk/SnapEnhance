626ea59ff606240f06928af9ec46c832be795f68
auth <64337177+authorisation@users.noreply.github.com>
2023-10-10 19:24:09 +0200

    fix(build): ci adjustments

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,10 @@ plugins {
     alias(libs.plugins.kotlinAndroid) apply false
 }
 
-rootProject.ext.set("appVersionName", "2.0.0")
-rootProject.ext.set("appVersionCode", 10)
+var versionName = "2.0.0"
+var versionCode = "0" //"0" for now until stable release
+
+rootProject.ext.set("appVersionName", versionName)
+rootProject.ext.set("appVersionCode", versionCode)
 rootProject.ext.set("applicationId", "me.rhunk.snapenhance")
 rootProject.ext.set("nativeName", properties["custom_native_name"] ?: java.security.SecureRandom().nextLong(1000000000, 99999999999).toString(16))
