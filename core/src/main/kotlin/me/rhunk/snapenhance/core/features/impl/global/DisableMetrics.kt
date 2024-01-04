39b62c7d40d330ded4d015d7a66b71f9fef749cb
auth <64337177+authorisation@users.noreply.github.com>
2024-01-04 03:17:49 +0100

    fix: conflict

--- core/src/main/kotlin/me/rhunk/snapenhance/core/features/impl/global/DisableMetrics.kt
+++ core/src/main/kotlin/me/rhunk/snapenhance/core/features/impl/global/DisableMetrics.kt
@@ -1,6 +1,5 @@
 package me.rhunk.snapenhance.core.features.impl.global
 
-import me.rhunk.snapenhance.core.event.events.impl.NativeUnaryCallEvent
 import me.rhunk.snapenhance.core.event.events.impl.NetworkApiRequestEvent
 import me.rhunk.snapenhance.core.features.Feature
 import me.rhunk.snapenhance.core.features.FeatureLoadParams
@@ -8,12 +7,6 @@ import me.rhunk.snapenhance.core.features.FeatureLoadParams
 class DisableMetrics : Feature("DisableMetrics", loadParams = FeatureLoadParams.INIT_SYNC) {
     override fun init() {
         if (!context.config.global.disableMetrics.get()) return
-
-        context.event.subscribe(NativeUnaryCallEvent::class) { event ->
-            if (event.uri == "/snapchat.cdp.cof.CircumstancesService/targetingQuery") {
-                event.canceled = true
-            }
-        }
 
         context.event.subscribe(NetworkApiRequestEvent::class) { param ->
             val url = param.url
