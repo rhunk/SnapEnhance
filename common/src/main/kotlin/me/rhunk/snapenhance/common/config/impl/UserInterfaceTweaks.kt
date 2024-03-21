0368cfaa2c19df9d5e0ec4cd8e714ac7e3a6925f
Jacob Thomas (2) <141059521+Theonethom@users.noreply.github.com>
2024-03-21 22:27:36 +0000

    Refactor

--- common/src/main/kotlin/me/rhunk/snapenhance/common/config/impl/UserInterfaceTweaks.kt
+++ common/src/main/kotlin/me/rhunk/snapenhance/common/config/impl/UserInterfaceTweaks.kt
@@ -25,9 +25,9 @@ class UserInterfaceTweaks : ConfigContainer() {
     }
 
     inner class CustomizeUi : ConfigContainer(hasGlobalState = true) {
-        val backgroundColour = string("background_colour", defaultString = #000000) { addNotices(FeatureNotice.UNSTABLE); requireRestart()}
-        val textColour = string("text_colour", defaultString = #ffffff) { addNotices(FeatureNotice.UNSTABLE); requireRestart()}
-        val drawablebackgroundColour = string("drawable_background_colour", defaultString = #000000) { addNotices(FeatureNotice.UNSTABLE); requireRestart()}
+        val backgroundColour = string("background_colour") { addNotices(FeatureNotice.UNSTABLE); requireRestart()}
+        val textColour = string("text_colour") { addNotices(FeatureNotice.UNSTABLE); requireRestart()}
+        val drawablebackgroundColour = string("drawable_background_colour") { addNotices(FeatureNotice.UNSTABLE); requireRestart()}
     }
         
     val amoledDarkMode = boolean("amoled_dark_mode") { addNotices(FeatureNotice.UNSTABLE); requireRestart() }
